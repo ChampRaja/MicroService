868b18698e03b8a556fafe780c6fc1154713c0fa
ChampRaja <rb211101@gmail.com>
2021-02-06 17:15:59 +0530

    departement-service

---
+++ departement-service/src/test/java/com/microservice/departement/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.departement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
